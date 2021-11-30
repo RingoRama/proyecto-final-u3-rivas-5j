bbb2274626e711aef8a577b14ef5d5bae8c25222
Jesús Renato Rivas Andrade <91618459+RingoRama@users.noreply.github.com>
2021-11-30 11:04:38 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 		<div class="wrapp-f">
             <p> Número de lista: 40 </p>
             <p> SUBMÓDULO 2 Desarrolla aplicaciones Web con conexión a bases de datos</p>
-            <p><a href="https://github.com/RingoRama/ProyectoRivas-Unidad2">Repositorio en GitHub</a> </p>
+            <p><a href="https://github.com/RingoRama/proyecto-final-u3-rivas-5j">Repositorio en GitHub</a> </p>
 		</div>
 </footer>
